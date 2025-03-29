8ce7a2d1972368964fb04fc1736c2cffcbf93d35
Courage Zeng <czeng@lcs.on.ca>
2025-03-29 15:20:51 -0400

    Added a model to match the structure of the JSON object received from our endpoint.

--- QuoteGenerator/Models/Quote.swift
+++ QuoteGenerator/Models/Quote.swift
@@ -5,4 +5,30 @@
 //  Created by 曾梓恒 on 2025-03-29.
 //
 
+import Foundation
 
+struct Quote: Identifiable, Codable {
+    let id: String  // Using quoteLink as the id
+    let quoteText: String
+    let quoteAuthor: String
+    
+    // Custom initializer to set id from quoteLink
+    init(
+        quoteText: String,
+        quoteAuthor: String,
+        quoteLink: String
+    ) {
+        self.id = quoteLink
+        self.quoteText = quoteText
+        self.quoteAuthor = quoteAuthor
+    }
+    
+    // Since quoteLink is now id, we don’t need it as a separate property
+}
+
+// Example
+let exampleQuote = Quote(
+    quoteText: "A man who doesn't trust himself can never really trust anyone else.",
+    quoteAuthor: "Cardinal Retz",
+    quoteLink: "http://forismatic.com/en/007ae94a6e/"
+)
