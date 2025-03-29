c08ab65a8119119c102e915146acd9474f83046f
Courage Zeng <czeng@lcs.on.ca>
2025-03-29 15:23:06 -0400

    Added a basic view model to hold the current joke that will be shown by the view.

--- QuoteGenerator/ViewModels/QuoteViewModel.swift
+++ QuoteGenerator/ViewModels/QuoteViewModel.swift
@@ -5,3 +5,26 @@
 //  Created by 曾梓恒 on 2025-03-29.
 //
 
+import Foundation
+ 
+@Observable
+class QuoteViewModel {
+    
+    // MARK: Stored properties
+    
+    // Whatever Quote has most recently been downloaded
+    // from the endpoint
+    var currentQuote: Quote?
+    
+    // MARK: Initializer(s)
+    init(currentQuote: Quote? = nil) {
+        
+        // Take whatever Quote was provided when an instance of
+        // this view model is created, and make it the current Quote.
+        //
+        // Otherwise, the default value for the current Quote
+        // will be a nil.
+        self.currentQuote = currentQuote
+    }
+    
+}
