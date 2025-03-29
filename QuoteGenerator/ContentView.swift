05c96edf4beb0262f390f1e31e9d57fa21cda02f
Courage Zeng <czeng@lcs.on.ca>
2025-03-29 14:53:47 -0400

    Initial Commit

---
+++ QuoteGenerator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuoteGenerator
+//
+//  Created by 曾梓恒 on 2025-03-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuoteGenerator/QuoteGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteGeneratorApp.swift
+//  QuoteGenerator
+//
+//  Created by 曾梓恒 on 2025-03-29.
+//
+
+import SwiftUI
+
+@main
+struct QuoteGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
