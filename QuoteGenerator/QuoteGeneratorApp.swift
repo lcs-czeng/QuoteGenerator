c053540468cc03acc70ac624f7869489a6799099
Courage Zeng <czeng@lcs.on.ca>
2025-03-29 15:12:45 -0400

    Initial project organization; added groups for MVVM design pattern.

--- QuoteGenerator/QuoteGeneratorApp.swift
+++ QuoteGenerator/QuoteGeneratorApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct QuoteGeneratorApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            QuoteView()
         }
     }
 }
---
+++ QuoteGenerator/ViewModels/QuoteViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  QuoteViewModel.swift
+//  QuoteGenerator
+//
+//  Created by 曾梓恒 on 2025-03-29.
+//
+
